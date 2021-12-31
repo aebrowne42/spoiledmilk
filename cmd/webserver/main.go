66bd5f8da0ff4930c3e63e23d5a03721a7d366cd
Austin <austin.browne1@gmail.com>
2021-12-31 02:26:28 -0600

    fuck

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/aebrowne42/spoiledmilk/pkg/qa"
+)
+
+func questionEndpoint(w http.ResponseWriter, req *http.Request) {
+	body, _ := io.ReadAll(req.Body)
+	fmt.Println(string(body))
+
+	if string(body) == "" {
+		fmt.Fprintf(w, "You need to ask me a question first, bitch")
+	} else {
+		fmt.Fprintf(w, qa.AnswerQuestion(string(body)))
+	}
+
+}
+
 func main() {
 
+	http.HandleFunc("/home", questionEndpoint)
+
+	http.ListenAndServe(":8090", nil)
 }
