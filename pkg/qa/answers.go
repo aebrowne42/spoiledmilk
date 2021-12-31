4489db322d3ec102529282c96202e47cf99eff94
aebrowne42 <austin.browne1@gmail.com>
2021-12-31 12:57:38 -0600

    added comment

--- pkg/qa/answers.go
+++ pkg/qa/answers.go
@@ -6,7 +6,7 @@ import (
 )
 
 func AnswerQuestion(question string) string {
-	//Create a seed to selecta random slice from a
+	//Create a seed to selecta random slice from answers
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 	return answers[r1.Intn(6)]
